2263ee19fa37e52f633aa931d2b5dd84f74d81db
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2023-01-07 22:50:12 +0530

    Update 02createFiles.py

--- 02createFiles.py
+++ 02createFiles.py
@@ -8,7 +8,6 @@ class createManyFiles:
 if __name__ == "__main__": 
     import os 
     filesList = ["test1.txt", "test1.xlsx", "test1.docx","test1.py"] 
-    # directorypath = /home/danielchakraborty/Desktop/
     directorypath = input("Please enter the directory path in which you want to create the files: ")
     for fileName in filesList: 
         filepath = directorypath + fileName 
